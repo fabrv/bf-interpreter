fc33e017ed1ba1e3c87084b40d10c74dca859e1e
Fabrizzio Rivera <frivera@nevicad.com>
2019-01-21 13:30:17 -0600

    Loops and input

--- src/cmds/Start.ts
+++ src/cmds/Start.ts
@@ -1,11 +1,12 @@
 import chalk from 'chalk'
 import * as fs from 'fs'
-import * as readLine from 'readline'
+import * as prompt from 'prompt-sync'
 class Start{
   code: string
   memory: Array<number> = []
   pointer: number = 0
   output: string = ''
+  loops: number = 0
   
   constructor(file:string){
     for (let i = 0; i < 32; i++){
@@ -24,8 +25,9 @@ class Start{
     })
   }
 
-  run(){
+  async run(){
     for (let i = 0; i < this.code.length; i++){
+      console.log(chalk.magenta(`${i}:${this.code[i]} | ${this.pointer}: ${this.memory[this.pointer]}`))
       switch (this.code[i]){
         case '<':
           if (this.pointer == 0){
@@ -43,31 +45,26 @@ class Start{
           break
         case '+':
           this.memory[this.pointer]++
+          break
         case '-':
           if (this.memory[this.pointer] == 0){
             console.error(chalk.red(`Negatives not allowed. Instruction ${i}:${this.code[i]}`))
             return
           }
+          this.memory[this.pointer]--
           break
         case '.':
           this.output += String.fromCharCode(this.memory[this.pointer])
           break
         case ',':
-          const rl = readLine.createInterface({
-            input: process.stdin,
-            output: process.stdout
-          });
-          rl.question('', (answer) => {
-            this.memory[this.pointer] = answer.charCodeAt(0)
-            rl.close();
-          });
+          this.memory[this.pointer] = prompt('INPUT:')
           break
 
 
         // LOOPS
         case '[':
           if (this.memory[this.pointer] == 0){
-            console.log(`Loop starts in ${i}`)
+            console.log(`Loop starts in ${i}: ${this.code[i]}`)
             let u = i
             let openLoops: number = 0
             while(this.code[u] != ']' && openLoops == 0){
@@ -75,15 +72,16 @@ class Start{
               if (this.code[u] == '['){openLoops++}
               if (this.code[u] == ']'){openLoops--}
             }
-            i = u--
+            i = u
 
             console.log(`Loop ends in ${i}`)
           }
           break
 
-        case ']':          
+        case ']':
+          console.log(this.memory[this.pointer])
           if (this.memory[this.pointer]!=0){
-            console.log(`Loop closed in ${i}`)
+            console.log(`Loop closed in ${i}: ${this.code[i]}`)
             let u = i
             let openLoops: number = 0
             while(this.code[u] != '[' && openLoops == 0){
@@ -91,8 +89,8 @@ class Start{
               if (this.code[u] == ']'){openLoops++}
               if (this.code[u] == '['){openLoops--}
             }
-            i = u--
-            console.log(`loop restarted in ${i}`)
+            i = u
+            console.log(`loop restarted in ${i}: ${this.code[i]}`)
           }
       }
     }
